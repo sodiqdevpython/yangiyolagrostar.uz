3bec03f34f4a9b67ca702675bf989240ab0171e6
Sodiq Oydinov <sodiqdevpython@gmail.com>
2025-04-30 21:19:32 +0500

    url moslandi

--- config/urls.py
+++ config/urls.py
@@ -2,16 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('i18n/', include('django.conf.urls.i18n')),
+    path('', include('main_app.urls')),
 ]
 
-urlpatterns += i18n_patterns(
-    path('', include('main_app.urls')),
-)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
