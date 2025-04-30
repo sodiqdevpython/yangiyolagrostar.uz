da4bd082fcbf447055291f46249fdea8589e4133
Sodiq Oydinov <sodiqdevpython@gmail.com>
2025-04-30 21:24:49 +0500

    settings moslandi

--- config/settings.py
+++ config/settings.py
@@ -120,9 +120,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 # STATICFILES_DIRS  = [ BASE_DIR / 'static' ]
 STATIC_ROOT = BASE_DIR / 'static'
+# STATICFILES_DIRS = BASE_DIR / 'staticfiles'
 
-# MEDIA_URL = 'media/'
-# MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
