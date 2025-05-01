ac7b7caa6fa3b0fb852108f74d4095a53a88219d
Sodiq Oydinov <sodiqdevpython@gmail.com>
2025-05-01 17:08:26 +0500

    medi muammosi hal qilindi

--- config/settings.py
+++ config/settings.py
@@ -124,7 +124,7 @@ STATIC_URL = 'static/'
 # STATICFILES_DIRS  = [ BASE_DIR / 'static' ]
 STATIC_ROOT = BASE_DIR / 'static'
 
-MEDIA_URL = 'medi/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
